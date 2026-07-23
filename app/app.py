c91bd539925e4ff344a3e75ff3eed3e99ab303d1
Anitta Vincent <anittavins@gmail.com>
2026-07-23 16:02:20 +0000

    Added Flask app and Dockerfile

--- app/app.py
+++ app/app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+print("Starting Flask...")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "AWS DevOps CI/CD Project Running"
+
+if __name__ == "__main__":
+    print("Running server...")
+    app.run(host="0.0.0.0", port=5000, debug=True)
